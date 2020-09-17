f1cebaf13c22e3a83519b5e5e0016310fffb1ea3
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-09-16 21:31:29 -0400

    added the beginnings of a pipeline

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,8 +1,34 @@
+## ==> Part 0: Loading
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 store = pd.HDFStore('../data/power_clean.h5')
 
-store['df_train].info()
-store['df_test'].info()
+## ==> Part 1: Exploration
+## Part 1a: Basic
+# store['df_train'].info()
+# store['df_test'].info()
+# store['df_train'].hist(bins=50)
 
+## Part 1b: Correlation
+# corr_matrix = store['df_train'].corr()
+# corr_matrix['Global_active_power'].sort_values(ascending=False)
+
+
+## ==> Part 2: Pipelines
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+pipe_electrical = Pipeline([
+    ('MinMax', MinMaxScaler())
+])
+
+pipe_date = Pipeline([
+    ('Std Scaler', StandardScaler())
+])
+
+pipe_full = ColumnTransformer([
+    ("elec", pipe_electrical
