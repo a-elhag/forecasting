1919a0a971501080aaf05d28eccdd5baba4ee83e
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-09-19 23:14:03 -0400

    sliding window to transformer class

--- src/pipeline.py
+++ src/pipeline.py
@@ -31,14 +31,24 @@ class SplitDate(BaseEstimator, TransformerMixin):
         return np.c_[year, month, day, hour, minute]
 
 class SlidingWindow(BaseEstimator, TransformerMixin):
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, window_size):
+        self.window_size = window_size
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
-        pass
+        '''
+        Creates a sliding window over an input that has the shape of
+        (rows, features) for X
+        '''
+        row_size, col_size = X.shape
+        X_out = np.zeros((row_size-self.window_size, 1))
+
+        for i in range(self.window_size):
+            X_out = np.concatenate((X_out, X[i:row_size-self.window_size+i]), axis=1)
+
+        return X_out[:, 1:]
 
 
 
@@ -48,16 +58,22 @@ attribs_elec = list(store['df_train'])[1:7]
 attribs_date = list(store['df_train'])[7]
 attribs_date = [attribs_date]
 
+pipe_Y = Pipeline([
+    ('min-max', MinMaxScaler()),
+])
+
 pipe_elec = Pipeline([
-    ('min-max', MinMaxScaler())
+    ('min-max', MinMaxScaler()),
+    ('window', SlidingWindow(3))
 ])
 
 pipe_date = Pipeline([
-    ('split date', SplitDate())
+    ('split date', SplitDate()),
+    ('window', SlidingWindow(3))
 ])
 
 pipe_full = ColumnTransformer([
-    ("Y", pipe_elec, attribs_Y),
+    ("Y", pipe_Y, attribs_Y),
     ("elec", pipe_elec, attribs_elec),
     ("date", pipe_date, attribs_date),
 ])
@@ -66,9 +82,11 @@ train_np = pipe_full.fit_transform(store['df_train'])
 test_np = pipe_full.transform(store['df_test'])
 
 train_X = train_np[:, 1:]
+train_X[:, 2].shape
 train_y = train_np[:, 0]
 
 test_X = test_np[:, 1:]
+test_X
 test_y = test_np[:, 0]
 
 # store.close()
@@ -85,19 +103,3 @@ mse_lin = mean_squared_error(test_y, test_y_predict)
 mse_lin = np.sqrt(mse_lin)
 
 pipe_full.named_transformers_['Y'].inverse_transform([[mse_lin]])
-
-## Function
-def windows(X, window_size):
-    '''
-    Creates a sliding window over an input that has the shape of
-    (rows, features) for X
-    '''
-    row_size, col_size = X.shape
-    X_out = np.zeros((row_size-window_size, 1))
-
-    for i in range(window_size):
-        X_out = np.concatenate((X_out, X[i:row_size-window_size+i]), axis=1)
-
-    return X_out[:, 1:]
-
-windows(e, 2)
