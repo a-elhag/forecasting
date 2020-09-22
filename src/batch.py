5ba1c88f07eaa080fa9dc8215a236c7684e0f5c0
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-09-21 23:22:15 -0400

    allowed it to be imported

--- src/batch.py
+++ src/batch.py
@@ -1,21 +1,22 @@
-# import pandas as pd
+import pandas as pd
 
 class BatchData():
-    def __init__(self, df_name, range_no):
+    def __init__(self, store, df_name, range_no):
+        self.store = store
         self.df_name = df_name
-        self.df_length = len(store[self.df_name])
+        self.df_length = len(self.store[self.df_name])
         self.range_no = range_no
         self.max_split = self.df_length//self.range_no
 
     def batch(self, split):
         idx1 = split*self.range_no
         idx2 = (split+1)*self.range_no
-        self.data = store[self.df_name].iloc[idx1:idx2, :].to_numpy()
+        self.data = self.store[self.df_name].iloc[idx1:idx2, :].to_numpy()
 
 if __name__ == '__main__':
     store = pd.HDFStore('../data/power_clean.h5')
-    train_batch = BatchData('df_train', 500000)
+    train_batch = BatchData(store, 'df_train', 500000)
     train_batch.batch(0)
 
-    test_batch = BatchData('df_test', 500000)
+    test_batch = BatchData(store, 'df_test', 500000)
     test_batch.batch(0)
