95871706a634c314958261e966c6000ef1c880f7
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-09-22 03:22:53 -0400

    optimized and added method

--- src/batch.py
+++ src/batch.py
@@ -1,20 +1,22 @@
 import pandas as pd
 
 class BatchData():
-    def __init__(self, store, df_name, range_no):
+    def __init__(self, store, range_no):
         self.store = store
-        self.df_name = df_name
-        self.df_length = len(self.store[self.df_name])
+        self.df_length = len(self.store)
         self.range_no = range_no
         self.max_split = self.df_length//self.range_no
 
     def batch(self, split):
         idx1 = split*self.range_no
         idx2 = (split+1)*self.range_no
-        self.data = self.store[self.df_name].iloc[idx1:idx2, :].to_numpy()
+        self.data = self.store.iloc[idx1:idx2, :].to_numpy()
 
     def full(self):
-        self.data = self.store[self.df_name].iloc[:, :].to_numpy()
+        self.data = self.store.iloc[:, :].to_numpy()
+
+    def names(self):
+        self.names = list(self.store)
 
 if __name__ == '__main__':
     store = pd.HDFStore('../data/power_clean.h5')
