b59ede14745f72a1497b2f3326ed2f74e33b6e0c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-10-04 14:03:06 -0400

    simple class made

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,5 @@
 ## Part 0: Loading
 from batch import BatchData
-
 import numpy as np
 import pandas as pd
 
@@ -70,7 +69,6 @@ class SlidingWindowY(BaseEstimator, TransformerMixin):
 
 class MyPipeline():
     def __init__(self, store, range_no):
-
         self.store = [store['df_train'], store['df_test']]
         self.df_length = [len(self.store[0]), len(self.store[1])]
         self.range_no = range_no
@@ -86,17 +84,24 @@ class MyPipeline():
 
 
     def data_batch(self, split, train_flag = True):
-        if train_flag:
-            idx1 = split*self.range_no[0]
-            idx2 = (split+1)*self.range_no[0]
+        self.split = split
+        self.train_flag = train_flag
+        self.data_trans = self.X = self.y = 0
+        if self.train_flag:
+            idx1 = self.split*self.range_no[0]
+            idx2 = (self.split+1)*self.range_no[0]
             self.data = self.store[0].iloc[idx1:idx2, :].to_numpy()
+
         else:
-            idx1 = split*self.range_no[1]
-            idx2 = (split+1)*self.range_no[1]
+            idx1 = self.split*self.range_no[1]
+            idx2 = (self.split+1)*self.range_no[1]
             self.data = self.store[1].iloc[idx1:idx2, :].to_numpy()
 
+
     def data_full(self, train_flag = True):
-        if train_flag:
+        self.train_flag = train_flag
+        self.data_trans = self.X = self.y = 0
+        if self.train_flag:
             self.data = self.store[0].iloc[:, :].to_numpy()
         else:
             self.data = self.store[1].iloc[:, :].to_numpy()
@@ -105,7 +110,6 @@ class MyPipeline():
         return list(self.store[0])
 
     def pipe_setting(self):
-
         self.attribs_Y = np.array([0])
         self.attribs_elec = np.arange(0, 7)
         self.attribs_date = np.array([7])
@@ -131,88 +135,76 @@ class MyPipeline():
         #     ("date", self.pipe_date, self.attribs_date),
         ])
 
-
-pipe = MyPipeline(store, [int(2e5), int(1e5)])
-pipe.data_batch(0, False)
-pipe.data.shape
-
+    def pre_fit(self, window_size, verbose = True):
+        if verbose:
+            print("Fitting Data")
 
 
-## Part 2: Something
-train_batch = BatchData(store['df_train'], 200000)
-train_batch.full()
-pipe_full.fit(train_batch.data)
+        self.data_full()
+        self.pipe_full.fit(self.data)
 
+        """
+        Need to do this because sklearn is acting like a *****, will always call 
+        fit_transform when you call fit. Thus we set window_size=1 initially and then
+        we go CRAZY after it
+        """
+        self.pipe_full.set_params(Y__window__window_size = window_size)
+        self.pipe_full.set_params(elec__window__window_size = window_size)
+      # self.pipe_full.set_params(date__window__window_size = window_size)
+        self.pipe_full.named_transformers_['Y']['window'].window_size = window_size
+        self.pipe_full.named_transformers_['elec']['window'].window_size = window_size
+      # self.pipe_full.named_transformers_['date']['window'].window_size = window_size
 
-## Part 3: Simple Reset
-def reset_window_size(pipe, window_size):
-    """
-    Need to do this because sklearn is acting like a *****, will always call 
-    fit_transform when you call fit. Thus we set window_size=1 initially and then
-    we go CRAZY after it
-    """
-    pipe.set_params(Y__window__window_size = window_size)
-    pipe.set_params(elec__window__window_size = window_size)
-#     pipe.set_params(date__window__window_size = window_size)
-    pipe.named_transformers_['Y']['window'].window_size = window_size
-    pipe.named_transformers_['elec']['window'].window_size = window_size
-#     pipe.named_transformers_['date']['window'].window_size = window_size
-    return pipe
+    def pre_transform(self, verbose = True):
+        if verbose:
+            if self.train_flag:
+                print(f"Transform Train: {self.split} out of {self.max_split[0]}")
+            else:
+                print(f"Transform Test: {self.split} out of {self.max_split[1]}")
 
-window_size = 60
-pipe_full = reset_window_size(pipe_full, window_size)
+        self.data_trans = self.pipe_full.transform(self.data)
+        self.X = self.data_trans[:, 1:]
+        self.y = self.data_trans[:, 0]
 
-## Part 4: Applying Pipelines
-train_batch.batch(1)
-train_np = pipe_full.transform(train_batch.data)
-train_batch.data.shape
+    def reverse_minmax(self, value):
+        if not isinstance(value, np.ndarray):
+            value = np.array([[value]])
 
-train_X = train_np[:, 1:]
-train_y = train_np[:, 0]
+        if value.ndim == 1:
+            value = value.reshape(1, -1)
+        
+        output = self.pipe_full.named_transformers_['Y']['min-max'].inverse_transform(value)
+        return output
 
-## Part 5: Batch Training Models
+pipe = MyPipeline(store, [int(1e5), int(1e5)])
+pipe.pre_fit(60)
+
+## Next
 from sklearn.linear_model import SGDRegressor
-
+from sklearn.metrics import mean_squared_error
 reg_sgd = SGDRegressor(verbose = 1, shuffle = False)
 
-for split in range(train_batch.max_split):
-    print(f"Split: {split} out of {train_batch.max_split}")
-    train_batch.batch(split)
-    train_np = pipe_full.transform(train_batch.data)
+for split in range(pipe.max_split[0]):
+    pipe.data_batch(split)
+    pipe.pre_transform()
+    reg_sgd.partial_fit(pipe.X, pipe.y)
 
-    train_X = train_np[:, 1:]
-    train_y = train_np[:, 0]
-
-    for _ in range(5):
-        reg_sgd.partial_fit(train_X, train_y)
-
-## Part 6: Testing Models
-from sklearn.metrics import mean_squared_error
-
-test_batch = BatchData(store['df_test'], 100000)
-test_batch.df_length
-test_batch.range_no
-test_batch.max_split
-remain = test_batch.df_length - (
-    (test_batch.max_split - 1) * test_batch.range_no)
-
+split = 0
 rmse_final = 0
-for split in range(test_batch.max_split):
-    print(f"Split: {split}")
-    test_batch.batch(split)
-    test_np = pipe_full.transform(test_batch.data)
 
-    test_X = test_np[:, 1:]
-    test_y = test_np[:, 0]
-
-    test_yhat = reg_sgd.predict(test_X)
-    mse = mean_squared_error(test_y, test_yhat)
+for split in range(pipe.max_split[1]):
+    pipe.data_batch(split, False)
+    pipe.pre_transform()
+    yhat = reg_sgd.predict(pipe.X)
+    mse = mean_squared_error(pipe.y, yhat)
     rmse = np.sqrt(mse)
-    rmse = pipe_full.named_transformers_['Y']['min-max'].inverse_transform([[rmse]])
-    if split == (test_batch.max_split-1):
-        rmse_final += (rmse[0][0] * remain)/test_batch.df_length
+    rmse = pipe.pipe_full.named_transformers_['Y']['min-max'].inverse_transform([[rmse]])
+
+    if split != (pipe.max_split[1]-1):
+        rmse_final += rmse[0][0] * pipe.range_no[1]/pipe.df_length[1]
     else:
-        rmse_final += rmse[0][0] * test_batch.range_no/test_batch.df_length
+        rmse_final += (rmse[0][0] * pipe.remain[1])/pipe.df_length[1]
+
 
 print(f" SGD Test rmse = {rmse_final}")
 
