6060d08a2509abe8df2801a68195c504c4090177
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-10-30 01:27:20 -0400

    finally succeeded in grouping

--- src/seasonality.py
+++ src/seasonality.py
@@ -13,4 +13,14 @@ df_train.set_index('DateTime', inplace=True)
 df_train_D = df_train.resample('D').mean().iloc[:, 0]
 df_train_H = df_train.resample('H').mean().iloc[:, 0]
 
-df_train.index.dayofyear
+df_train_D_idx = df_train_D.index.dayofyear
+df_train_H_idx = df_train_H.index.hour
+
+df_train_D_idx = df_train_D.index.dayofyear.isin([365])
+df_train_D.loc[df_train_D_idx]
+
+for day in range(1, 367):
+    if day > 364:
+        print(day)
+
+
