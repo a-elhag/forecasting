eb33d132bed18737c3e4c274664079354394306c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-10-29 21:58:39 -0400

    finally got how to resample

--- src/seasonality.py
+++ src/seasonality.py
@@ -4,3 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 store = pd.HDFStore('../data/power_clean.h5')
+df_train = store['df_train']
+df_test = store['df_test']
+store.close()
+
+## Part 1: Formatting Data
+df_train.set_index('DateTime', inplace=True)
+df_train.resample('3T').mean()
